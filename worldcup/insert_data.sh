c8fff5bec77238be0fe10ad5523ac741d62a5b02
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-17 21:32:06 +0200

    insert worldcup teams from csv

--- worldcup/insert_data.sh
+++ worldcup/insert_data.sh
@@ -8,7 +8,21 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE teams, games");
+
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  echo Year: $YEAR, Winner: $WINNER, OPPONENT: $OPPONENT
+  if [[ $YEAR != year ]]
+  then
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+    fi
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+    fi
+  fi
 done
