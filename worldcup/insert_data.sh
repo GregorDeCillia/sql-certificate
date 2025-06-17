5b52e4254529708fc15c418fe70939b4473a4424
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-17 21:12:40 +0200

    setup loop for team inserts

--- worldcup/insert_data.sh
+++ worldcup/insert_data.sh
@@ -8,4 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  echo Year: $YEAR, Winner: $WINNER, OPPONENT: $OPPONENT
+done
