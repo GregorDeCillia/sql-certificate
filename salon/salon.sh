d4726396b8ffd65b9108ce603e944b1b670f18ba
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-18 06:46:35 +0200

    pass all tests

--- salon/salon.sh
+++ salon/salon.sh
@@ -28,3 +28,23 @@ SELECT_SERVICE() {
 }
 
 SELECT_SERVICE
+
+echo -e "\nPlease enter your phone number"
+read CUSTOMER_PHONE
+CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+if [[ -z $CUSTOMER_NAME ]]
+then
+  echo -e "\nwhat is your name?"
+  read CUSTOMER_NAME
+  INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(phone,name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+fi
+
+echo -e "\nwhat time would yo like to book?"
+read SERVICE_TIME
+
+SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED");
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'");
+
+INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')")
+
+echo "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
